6adac16ac372691299d5144011dc3d1843020140
yousuf <yosefmahmoud356@gmail.com>
2023-06-02 18:21:46 +0300

    mainthread solved

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ class scrz(F.Screen):pass
 
 class Main(App):
     event = Event()
+    cont = None
     # def __init__(self,*args,**kwargs):
     #     super().__init__(*args,**kwargs)
         
@@ -25,39 +26,44 @@ class Main(App):
 
         def a():
             lenth = 100
-            for x in  range(50):
+            for x in  range(5):
                 self.add_tab(x)
-                cont = self.add_container(x)
-                print(cont)
+                self.add_container(x)
+                while not self.cont:
+                    sleep(.001) 
+                    if self.event.is_set():
+                        return
+                print(self.cont)
                 for i in range(lenth):
                     #sleep(1)
                     if self.event.is_set():
                         return
                     sleep(.0001)
-                    self.add_card(i, cont)
-        #Thread(target=a).start()
-        a()
+                    self.add_card(i, self.cont)
+                self.cont = None
+        Thread(target=a).start()
+        # a()
         #self.root.ids.bar.scroll_to(self.root.ids.test)
     def on_stop(self):
         self.event.set()
-    #@mainthread
+    @mainthread
     def add_card(self, i, widget):
         widget.add(Card(text = str(i)))
-   # @mainthread
+    @mainthread
     def add_widget1(self, widget, child):
         widget.add_widget(child)
-    #@mainthread
+    @mainthread
     def add_tab(self, name):
         print(name, type(name))
         tb = Tab(text = str(name))
         self.root.ids.bar_box.add_widget(tb)
-   # @mainthread
+    @mainthread
     def add_container(self, x):
-        cont = RContainer()
-        print(cont, x)
+        self.cont = RContainer()
+        print(self.cont, x)
         scr=scrz(name = str(x))
         self.add_widget1(self.root.ids.scrz_manager, scr)
-        scr.add_widget(cont)
-        return cont
+        scr.add_widget(self.cont)
+        
 main=Main()
 main.run()
